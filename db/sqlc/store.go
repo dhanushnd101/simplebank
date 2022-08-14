856889b20766f372b84ff91607c29200550543b8
Dhanush Dinesh <dhanushndinesh@gmail.com>
2022-08-13 22:40:54 -0400

    first commit

---
+++ db/sqlc/store.go
@@ -0,0 +1,132 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+// stores previous all func to execut db queries and tranatios
+type Store struct{
+	*Queries
+	db *sql.DB
+}
+
+
+//  creates a new Store 
+func NewStore(db *sql.DB) *Store{
+	return &Store{
+		db:db,
+		Queries: New(db),
+	}
+}
+
+// execTx executes the fucntion within a database transatcation
+func (store *Store) execTx(ctx context.Context,fn func(*Queries) error) error{
+	tx,err := store.db.BeginTx(ctx, nil)
+	if err != nil{
+		return err
+	}
+
+	q :=New(tx)
+	err = fn(q)
+	if err != nil{
+		if rbErr := tx.Rollback(); rbErr != nil{
+			return fmt.Errorf("tx err: %v, rb err: %v",err,rbErr)
+		}
+		return err
+	}
+
+	return tx.Commit()
+}	
+
+// TransferTxParams contains theinput parements of the transfer transaction
+type TransferTxParams struct{
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount   	  int64 `json:"amont"`
+}
+
+//  TransferTxResult is the result of the transfer transaction
+type TransferTxResult struct{
+	 Transfer Transfer `json:"transfer"`
+	 FromAccount Account `json:"from_account"`
+	 ToAccount Account `json:"to_account"`
+	 FromEntry Entry `json:"from_entry"`
+	 ToEntry Entry `json:"to_entry"`
+}
+
+var txKey =struct{}{}
+
+
+// TransferTx performs a money transfer from one account to the other 
+// It creates a transfer recored , add account entries and update accounts balance within a single databe transacation
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams)(TransferTxResult,error){
+	var result TransferTxResult
+	err := store.execTx(ctx, func(q *Queries) error{
+		var err error
+
+		// txName := ctx.Value(txKey)
+
+		// fmt.Println(txName,"creare transfer")
+		result.Transfer, err = q.CreateTransfers(ctx, CreateTransfersParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID: arg.ToAccountID,
+			Amount: arg.Amount,
+		})
+		if err!= nil{
+			return err
+		}
+
+		// fmt.Println(txName,"creare entry 1")
+		result.FromEntry, err = q.CreateEntries(ctx,CreateEntriesParams{
+			AccountID: arg.FromAccountID,
+			Amount: -arg.Amount,
+		})
+		if err!= nil{
+			return err
+		}
+
+
+		// fmt.Println(txName,"creare entry 2")
+		result.ToEntry, err = q.CreateEntries(ctx,CreateEntriesParams{
+			AccountID: arg.ToAccountID,
+			Amount: arg.Amount,
+		})
+		if err!= nil{
+			return err
+		}
+
+		// getaccount -> update account balance
+
+		// fmt.Println(txName,"update account 1")
+		if(arg.FromAccountID < arg.ToAccountID){
+			result.FromAccount, result.ToAccount,err = addMoney(ctx,q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
+		}else{
+			result.ToAccount, result.FromAccount,err = addMoney(ctx,q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
+		}
+		return nil
+	})
+	return result, err
+
+}
+func addMoney(
+	ctx context.Context,
+	q *Queries,
+	accountID1 int64,
+	amount1 int64,
+	accountID2 int64,
+	amount2 int64,
+)(account1 Account, account2 Account, err error){
+	account1,err = q.AddAccountBalance(ctx,AddAccountBalanceParams{
+		ID: accountID1,
+		Amount: amount1,
+	})
+	if err != nil{return }
+
+	account2,err = q.AddAccountBalance(ctx,AddAccountBalanceParams{
+		ID: accountID2,
+		Amount: amount2,
+	})
+	return 
+}
---
+++ db/util/random.go
@@ -0,0 +1,45 @@
+package util
+import(
+	"math/rand"
+	"time"
+	"strings"
+)
+const alphabet="abcdefghijklmnopqrstuvwxyz"
+
+func init(){
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RandomeInt generates a random int between min and max
+func RandomeInt(min, max int64) int64{
+	return min+rand.Int63n(max-min+1) //min -max
+}
+
+// RandomeString generates a random string lenght n 
+func RandomeString(n int) string{
+	var sb strings.Builder
+	k:= len(alphabet)
+
+	for i := 0; i<n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+	return sb.String()
+}
+
+// RandomOwner generates the owner name 
+func RandomOwner() string{
+	return RandomeString(6)
+}
+
+//  RandomMoney gernrates random money
+func RandomMoney() int64{
+	return RandomeInt(0,1000)
+}
+
+// RandomCurrency generates random currency
+func RandomCurrency() string{
+	currency :=[]string{"USD","EUR","INR"}
+	n:= len(currency)
+	return currency[rand.Intn(n)]
+}
